4e84d48059166fb35a46b88befae7468fd50a1db
Arnold Noronha <arnstein87@gmail.com>
2009-08-04 21:28:42 -0400

    GPL licence.

--- lcat.c
+++ lcat.c
@@ -1,3 +1,23 @@
+/**
+ * @file lcat.c 
+ * 
+ * Copyright (C) 2009 Arnold Noronha <arnstein87 AT gmail DOT com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version. 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -90,7 +110,6 @@ char* parse (const char* buf, const char* query) {
 	char *token = strtok (temp, " \t");
 
 	if (strcmp (token, "tcp") != 0) {
-		fprintf (stderr, "not tcp\n");
 		free (temp);
 		return NULL;
 	}
