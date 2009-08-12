5c005849b70e2b05c2fc22b1e7a3fbbc0e5c76c8
Arnold Noronha <arnstein87@gmail.com>
2009-08-12 13:29:08 -0400

    exporting the interface.

--- io_loop.h
+++ io_loop.h
@@ -24,6 +24,8 @@ typedef void (*io_timeout) ();
 
 void io_loop_add_fd (int fd, io_callback cb, void* _userdata);
 
+void io_loop_add_fd_write (int fd, io_callback cb, void* _userdata);
+
 void io_loop_remove_fd (int fd);
 
 void io_loop_set_timeout (int seconds, io_timeout cb);
