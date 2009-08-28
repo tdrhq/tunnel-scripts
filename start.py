701d9d537cb3b195eb34eff4ed74633fdc4e3ae6
Arnold Noronha <arnstein87@gmail.com>
2009-08-28 11:57:36 -0400

    start.py changes, it actually works ;)

--- start.py
+++ start.py
@@ -2,12 +2,13 @@
 
 import os
 import subprocess
+import signal
 
 # so here's the deal, user gives
 class Tunnel:
     ssh_gateway = '61.12.4.27'
-    ssh_gateway_user = 'narnold'
-    ssh_D_port = 3128 # user doesn't really need to know about this
+    ssh_gateway_user = 'arnold'
+    ssh_D_port = 8000 # user doesn't really need to know about this
     tsocks = '/usr/bin/tsocks'
     port = 8100 # this will the local port iptables maps to
     interface = 'wlan0'
@@ -33,7 +34,7 @@ class Tunnel:
             self.rule)
 
     def start_ssh_tunnel (self):
-        ssh_command = ['ssh', '-D', str(self.ssh_D_port), '-l', 
+        ssh_command = ['ssh', '-N', '-D', str(self.ssh_D_port), '-l', 
                        self.ssh_gateway_user, self.ssh_gateway]
 
         # do we become another user before doing this? 
@@ -43,16 +44,60 @@ class Tunnel:
         self.ssh_process = subprocess.Popen (ssh_command)
 
     def end_ssh_tunnel (self):
-        self.ssh_process.terminate ()
-        self.ssh_process.kill ()
+        # self.ssh_process.terminate ()
+        # self.ssh_process.kill ()
+        print "Killing SSH tunnel"
+        os.kill (self.ssh_process.pid, signal.SIGKILL)
+        self.ssh_process = None
 
     def restart_ssh_tunnel (self):
         self.end_ssh_tunnel ()
         self.restart_ssh_tunnel ()
 
-    
+
+    def build_tsocks_config (self):
+        self.tsocks_config_file = os.tempnam ()
+        f = open (self.tsocks_config_file, "w")
+        f.write ("""
+server = 127.0.0.1
+server_type = 5
+server_port = %s
+""" % self.ssh_D_port);
+        f.close ()
+        print "here\n"
+        print self.tsocks_config_file 
+        
+    def cleanup_tsocks_config (self):
+        os.unlink (self.tsocks_config_file)
+        self.tsocks_config_file = None
+
+    def run_lcat (self):
+        # if your tsocks is not built with ALLOW_ENV_CONFIG, you're in
+        # trouble, and I have no way of detecting it!
+        lcat_args = ['./lcat', '-p', str(self.port)]
+        if (self.sudo_user != None):
+            lcat_args = ['sudo', '-u', self.sudo_user] + lcat_args
+
+        os.putenv ('TSOCKS_CONF_FILE', self.tsocks_config_file);
+        subprocess.call (["/usr/bin/env"])
+        ret = subprocess.call (lcat_args)
+        #and we're done!
+        print "lcat terminated with %d" % ret
+        
+    def everything (self):
+        self.start_iptables ()
+        self.start_ssh_tunnel ()
+        self.build_tsocks_config ()
+
+        try:
+            self.run_lcat ()
+        except KeyboardInterrupt:
+            pass
+        
+        self.cleanup_tsocks_config ()
+        self.end_ssh_tunnel ()
+        self.end_iptables ()
+
+
 tunnel = Tunnel ()
-tunnel.start_iptables ()
-tunnel.end_iptables ()
-
-
+tunnel.everything ()
